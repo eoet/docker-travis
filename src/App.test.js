bf42fe5e4b292fb3846d71325d5ebcedc6fbab1f
Hiep Nguyen <hoanghiep2004hy@gmail.com>
2020-12-13 11:13:32 +0700

    Fix test

--- src/App.test.js
+++ src/App.test.js
@@ -3,11 +3,11 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/I was changed on the Feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
 test('renders learn react link 1', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/I was changed on the Feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
